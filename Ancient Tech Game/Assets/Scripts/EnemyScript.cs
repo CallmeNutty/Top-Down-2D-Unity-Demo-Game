a704db33fdf49c942168c618880886932cbf4d03
TheNuttyGamer <33847045+TheNuttyGamer@users.noreply.github.com>
2017-12-07 20:59:47 +0000

    Initial

---
+++ Ancient Tech Game/Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyScript : MonoBehaviour {
+
+    private bool targetingCivillian;
+    [SerializeField]
+    private float runSpeed;
+    
+    private GameObject chosenCivillian;
+    private GameObject[] civillians;
+
+
+    private void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject == chosenCivillian)
+        {
+            Destroy(coll.gameObject);
+            Destroy(this.gameObject);
+        }
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+        civillians = GameObject.FindGameObjectsWithTag("Civillian");
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (targetingCivillian == false)
+        {
+            targetingCivillian = true;
+            chosenCivillian = civillians[Random.Range(0, civillians.Length)];
+        }
+        else if (targetingCivillian == true)
+        {
+            gameObject.transform.position = Vector3.Lerp(gameObject.transform.position, chosenCivillian.transform.position, runSpeed);
+            Debug.Log("HI");
+        }
+
+        Debug.Log(chosenCivillian);
+        Debug.Log(targetingCivillian);
+    }
+}
