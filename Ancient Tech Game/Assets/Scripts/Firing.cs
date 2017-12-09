ec8fc15bc0d68add15ef7c742465ce2b4e346e1d
TheNuttyGamer <33847045+TheNuttyGamer@users.noreply.github.com>
2017-12-09 23:10:00 +0000

    End Of Day

--- Ancient Tech Game/Assets/Scripts/Firing.cs
+++ Ancient Tech Game/Assets/Scripts/Firing.cs
@@ -20,20 +20,17 @@ public class Firing : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
     {
-	    if (Input.GetMouseButtonDown(0))
+        
+
+        Vector3 pos = Camera.main.WorldToScreenPoint(transform.position);
+        Vector3 dir = Input.mousePosition - pos;
+
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        bulletSpawner.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+        if (Input.GetMouseButtonDown(0))
         {
             Instantiate(bullet, new Vector3(bulletSpawner.transform.position.x, bulletSpawner.transform.position.y), bulletSpawner.transform.rotation, emptyBullet.transform);
         }
-
-        Vector3 mousePos = Input.mousePosition;
-        mousePos.z = 1f;
-
-        Vector3 objectPos = Camera.main.WorldToScreenPoint(transform.position);
-        mousePos.x = mousePos.x - objectPos.x;
-        mousePos.y = mousePos.y - objectPos.y;
-
-        float angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
-        bulletSpawner.transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
-
     }
 }
