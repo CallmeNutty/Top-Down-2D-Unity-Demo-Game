6b69f5e2eed6dc552a93938432572a9130620027
TheNuttyGamer <33847045+TheNuttyGamer@users.noreply.github.com>
2018-01-15 18:26:45 +0000

    Finished Wave Timer and Random Spawns
    Difficulty scaling not implemented yet

--- Ancient Tech Game/Assets/Scripts/TurnManager.cs
+++ Ancient Tech Game/Assets/Scripts/TurnManager.cs
@@ -1,37 +1,108 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
+using System.Collections.Generic;
 
 public class TurnManager : MonoBehaviour {
 
     //Declare Variables
+    private List<GameObject> mobList = new List<GameObject>();
     [SerializeField]
     private Text waveCounter;
+    [SerializeField]
+    private GameObject slime;
+    [SerializeField]
+    private GameObject golem;
 
-    private int wave = 1;
-    private float counter = 5;
+    public int enemiesThisWave;
+    private bool waveOver;
+    private int wave = 0;
+    public float counter = 5;
+    public int prepTime = 5;
 	
 	// Update is called once per frame
 	void Update ()
     {
-        //Stop the timer from going below zero
-        if (counter > 0)
+        //If wave is over
+        if (TrackMobs.enemies.Count == 0 && Mathf.Round(counter) < prepTime && waveOver != true)
+        {
+            wave++;
+            waveOver = true; //Prevent this tree from running again until next wave
+            counter += prepTime; //Reset the counter
+            
+            //Adds random enemies to the Moblist
+            for (int k = 0; k < enemiesThisWave; k++)
+            {
+                mobList.Add(PickEnemies(slime, golem, 0.8f, 0.2f));
+            }
+        }
+        //If timer has run out
+        else if (counter <= 0)
+        {
+            waveOver = false; //Allow first tree to run again
+
+            for (int k = 0; k < mobList.Count; k++)
+            {
+                Instantiate(mobList[k], PickSpawnPoint(-20, 20, 20, -20, 30, 10), Quaternion.identity);
+            }
+        }
+        //If counter hasn't run out yet
+        else if (counter > 0)
         {
             counter -= Time.deltaTime; //Reduce counter by one frame
         }
 
-        //If wave is over
-        if (TrackMobs.enemies.Count == 0)
+        //To change the top text properly
+        waveCounter.text = string.Format("Wave {0} starts in: {1}", wave ,Mathf.Round(counter));
+    }
+
+    //Method which picks random GameObject based on percentage chance
+    GameObject PickEnemies(GameObject slime, GameObject golem, float slimeChance, float golemChance)
+    {
+        if (Random.value <= slimeChance)
         {
-            StartCounter(5, counter);
+            print("slime");
+            return slime;
+        }
+        else if (Random.value <= golemChance)
+        {
+            print("golem");
+            return golem;
         }
 
-        waveCounter.text = "Wave 0 starts in: " + Mathf.Round(counter);
+        return slime;
     }
 
-    void StartCounter (int timeAmount, float counter)
+    //Method which picks a random point in a given range of X and Y
+    Vector3 PickSpawnPoint(int leftY, int rightY, int topX, int bottomX, int YRange, int XRange)
     {
-        counter += timeAmount;
-    }
+        float yPos;
+        float xPos;
 
+        bool spawnLeft = Random.Range(1, 3) == 1;
+        bool spawnTop = Random.Range(1, 3) == 1;
+
+        if (spawnLeft == true)
+        {
+            yPos = Random.Range(leftY - YRange, leftY + YRange);
+        }
+        else
+        {
+            yPos = Random.Range(rightY - YRange, rightY + YRange);
+        }
+
+        if (spawnTop == true)
+        {
+            xPos = Random.Range(topX - XRange, topX + XRange);
+        }
+        else
+        {
+            xPos = Random.Range(bottomX - XRange, bottomX + XRange);
+        }
+
+        print("xpos = " + xPos);
+        print("ypos = " + yPos);
+
+        return new Vector3(xPos, yPos);
+    }
 }
